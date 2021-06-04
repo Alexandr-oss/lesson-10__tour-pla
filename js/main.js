9a3ce55e11acf14adb458eff3bfeda28b5dd151e
Alexandr-oss <Aleksandarbabaleu@yandex.ru>
2021-06-04 13:38:20 +0400

    create: modalWindow

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const swiper = new Swiper('.swiper-container', {
+$(document).ready(function(){
+  const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
 
@@ -22,5 +23,24 @@ const reviews = new Swiper('.reviews-slider', {
 var menuButton = document.querySelector(".menu-button");
 
 menuButton.addEventListener('click', function(){
-  console.log('Клик по кнопке меню')
+
+});
+
+var modalButton =$("[data-toggle=modal]");
+var closeModalButton =$(".modal__close");
+modalButton.on("click", openModal);
+closeNodalButton.on("click", closeModal);
+
+function openModal() {
+  var targetModal = $(this).attr("data-href");
+  $(targetModal).find(".modal__overlay").addClass(".modal__overlay--visible");
+  $(targetModal).find(".modal__dialog").addClass(".modal__dialog--visible");
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay =$(".modal__overlay");
+  var modalDialog =$(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+}
 });
