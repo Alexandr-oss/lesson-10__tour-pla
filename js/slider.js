02dc1b8cbffe348c26edd8b8b843481f6cd94e75
Alexandr <aleksandarbabaleu@yandex.ru>
2022-10-21 12:19:02 +0300

    fix: footer

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  const swiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".slider-button--next",
+      prevEl: ".slider-button--prev",
+    },
+  });
+  const reviews = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+});
